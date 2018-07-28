609594121b9e32ff77a3b32304e0e1fed6ed00ea
tendayling <tendaysling@gmail.com>
2018-07-28 23:58:15 +0800

    set some file for testing commit

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDFmsxrwMRcTrhXFI_4VhGlqHYFll-h8BE",
+    authDomain: "practicing-lhliao-18-0723.firebaseapp.com",
+    databaseURL: "https://practicing-lhliao-18-0723.firebaseio.com",
+    projectId: "practicing-lhliao-18-0723",
+    storageBucket: "practicing-lhliao-18-0723.appspot.com",
+    messagingSenderId: "275078980074"
+  };
+  firebase.initializeApp(config);
