46b186a86b430e29e979d2d70f436944f322d899
lhliao <lhliao@g.sce.pccu.edu.tw>
2018-08-01 16:20:22 +0800

    finished homework

--- js/firebase.js
+++ js/firebase.js
@@ -1,10 +1,105 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDFmsxrwMRcTrhXFI_4VhGlqHYFll-h8BE",
-    authDomain: "practicing-lhliao-18-0723.firebaseapp.com",
-    databaseURL: "https://practicing-lhliao-18-0723.firebaseio.com",
-    projectId: "practicing-lhliao-18-0723",
-    storageBucket: "practicing-lhliao-18-0723.appspot.com",
-    messagingSenderId: "275078980074"
-  };
-  firebase.initializeApp(config); 
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDFmsxrwMRcTrhXFI_4VhGlqHYFll-h8BE",
+  authDomain: "practicing-lhliao-18-0723.firebaseapp.com",
+  databaseURL: "https://practicing-lhliao-18-0723.firebaseio.com",
+  projectId: "practicing-lhliao-18-0723",
+  storageBucket: "practicing-lhliao-18-0723.appspot.com",
+  messagingSenderId: "275078980074"
+};
+
+firebase.initializeApp(config); 
+
+var Record = firebase.database().ref('homeWork-BMI-(ByLiao)')
+// var data = {}
+
+var heighCm = document.getElementById('height')
+var weightKg = document.getElementById('weight')
+var list = document.getElementById('list')
+var send = document.getElementById('yellow-btn')
+var redo = document.getElementById('redo-btn')
+var resultInBtn = document.getElementById('report-in-btn')
+var resultInHeader = document.getElementById('result-in-header')
+var recordTime = (new Date).getFullYear() + "-" + ((new Date).getMonth()+1) + "-"+ (new Date).getDate()
+
+var outerThis = this
+var colorOfLine = ''
+var BMILav = ''
+
+Record.once('value', function(snapshot){
+  var data = snapshot.val()
+  snapshot.forEach(element => {
+    let item = element.val()
+    let addLi = document.createElement("li")
+    let newItem = "<div class="+ item.class +"></div><div><p><span>"+ item.BMILav +"</span></p><p>BMI<span>"+ item.bmiNum +"</span></p><p>weight<span>"+ item.kg +"kg</span></p><p>heigh<span>"+ item.cm + "cm</span></p><p>"+ item.date + "</p></div>"
+    addLi.innerHTML = newItem
+    list.insertBefore(addLi,list.childNodes[0])
+  });
+})
+
+redo.addEventListener('click', function(){
+  send.classList.remove('sended')
+  send.classList.remove(colorOfLine)
+  resultInBtn.classList.remove('pushed')
+  resultInBtn.textContent = '看結果'
+  heighCm.value = ''
+  weightKg.value = ''
+  resultInHeader.textContent = ''
+  resultInHeader.classList.remove(colorOfLine)
+  event.stopPropagation()
+})
+
+send.addEventListener('click', function(e){
+  var typeOfHeigh = parseFloat(heighCm.value).toString();
+  var typeOfWeight = parseFloat(weightKg.value).toString();
+
+  if(typeOfHeigh == 'NaN' || typeOfWeight == 'NaN'){
+    // console.log('你是不知道欄位都要輸入數字嗎?')
+    alert('欄位都要輸入數字喔')
+  }else if(send.classList.contains('sended')){
+    console.log('現在不能按')
+    
+  }else{
+    // console.log({'cm': heighCm.value, 'kg': weightKg.value, 'date': recordTime})
+    outerThis.showRecord()
+    send.classList.add('sended')
+    send.classList.add(colorOfLine)
+    console.log(colorOfLine)
+    console.log(BMILav)
+  }
+})
+
+var showRecord = function(){
+  var bmiNum = Math.round(weightKg.value/((heighCm.value/ 100)*(heighCm.value/ 100))*10) / 10
+
+  if(bmiNum < 18.5){
+    colorOfLine = 'green-line'
+    BMILav = '過輕'
+  }else if(bmiNum >= 18.5 && bmiNum < 24){
+    colorOfLine = 'blue-line'
+    BMILav = '正常'
+  }else if(bmiNum >= 24 && bmiNum < 27){
+    colorOfLine = 'orange-line'
+    BMILav = '過重'
+  }else if(bmiNum >= 27 && bmiNum < 30){
+    colorOfLine = 'orange-line'
+    BMILav = '輕度肥胖'
+  }else if(bmiNum >= 30 && bmiNum < 35){
+    colorOfLine = 'fat-orange-line'
+    BMILav = '中度肥胖'
+  }else if(bmiNum >= 35){
+    colorOfLine = 'red-line'
+    BMILav = '重度肥胖'
+  }
+
+  Record.push({'BMILav': BMILav, 'bmiNum': bmiNum, 'cm': heighCm.value, 'kg': weightKg.value, 'date': recordTime, 'class': colorOfLine})
+
+  let addLi = document.createElement("li")
+  let newItem = "<div class="+ colorOfLine +"></div><div><p><span>"+ BMILav +"</span></p><p>BMI<span>"+ bmiNum +"</span></p><p>weight<span>"+ weightKg.value +"kg</span></p><p>heigh<span>"+ heighCm.value + "cm</span></p><p>"+ recordTime + "</p></div>"
+  addLi.innerHTML = newItem
+  list.insertBefore(addLi,list.childNodes[0])
+  resultInBtn.classList.add('pushed')
+  resultInBtn.innerHTML = bmiNum + '<br><span>BMI</span>'
+  resultInHeader.classList.add(colorOfLine)
+  resultInHeader.textContent = BMILav
+}
